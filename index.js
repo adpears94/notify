cdeaf32b14dc3c9ae993af7547bf6fd87572c3f7
adpears94 <pears.avery@gmail.com>
2023-08-11 10:17:15 -0400

    html in api

--- index.js
+++ index.js
@@ -12,14 +12,28 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const htmlResponse = `
+<html>
+    <head>
+        <title>Notification API</title>
+    </head>
+    <body>
+        <h1>Welcome to the Notification API!</h1>
+        <ul>
+            <li><code>/api/pull</code> is for receiving notifications and messages.</li>
+            <li><code>/api/send</code> is for sending notifications through the service.</li>
+        </ul>
+    </body>
+</html>`;
+
 
 const appriseUrl = "http://localhost:7070/apprise/notify"; // replace with your apprise server url if not localhost
 
 
 
 // --------- initial API Routes ------------------------------
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
+app.get("/api", (req, res) => {
+  res.send(htmlResponse);
 });
 
 
