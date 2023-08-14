f4ba9e2e32e93555707d286a82927303efce122d
adpears94 <pears.avery@gmail.com>
2023-08-14 08:41:54 -0400

    updated pull

--- controllers/pull.js
+++ controllers/pull.js
@@ -9,21 +9,30 @@ const test = (req, res) => {
 const pull = async (req, res) => {
     const TOKEN = '5jixnf6a5bfc7fq7hb87wp5cjh'
 
-   const {channel_id, channel_name, team_domain, team_id, post_id, text, timestamp, token, trigger_words, user_id, user_name} = req.body
+//    const {channel_id, channel_name, team_domain, team_id, post_id, text, timestamp, token, trigger_words, user_id, user_name} = req.body
 
+   const { token } = req.body;  // Destructure only what you use. You can add others as needed.
+    console.log('it has hit the endpoint')
+    console.log(req.body)
+   // First, validate the token
+   if (token !== TOKEN) {
+       return res.status(401).send({ message: 'Unauthorized' });
+   }
 
-    try {
-        const response = await axios.post('https://notifyme-fezl.onrender.com/api/pull')
-        if (req.body !== TOKEN) {
-            res.status(401).send({message: 'Unauthorized'})
-        }
-        console.log(req.body)
-        res.status(200).send({message: 'Messages Received!'})
+   try {
+       // Log the body here if you want to see it even if axios throws an error
+       console.log(req.body);
+       
+       // If you need to send data with the POST request, ensure you provide it
+       const axiosResponse = await axios.post( )
+         
+       console.log(axiosResponse)
+       res.status(200).send({ message: 'Messages Received!' });
 
-    } catch (e) {
-        console.log(e)
-         res.status(500).send({message: 'Error This is a post request'})
-    }
+   } catch (e) {
+       console.error(e);  // Use console.error for errors.
+       res.status(500).send({ message: 'Error This is a post request' });
+   }
 }
 
 const pull2 = async (req, res) => {
