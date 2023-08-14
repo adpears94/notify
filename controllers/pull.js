fe45c5292ba338b582bca2fb7f4ac8d26d95332b
adpears94 <pears.avery@gmail.com>
2023-08-14 10:14:46 -0400

    update 2'

--- controllers/pull.js
+++ controllers/pull.js
@@ -1,10 +1,6 @@
 const axios = require('axios');
 const { json } = require('body-parser');
 
-const test = (req, res) => {
-    res.json({ message: "Hello World!" });
-}
-
 
 const pull = async (req, res) => {
     const TOKEN = '5jixnf6a5bfc7fq7hb87wp5cjh'
@@ -35,13 +31,5 @@ const pull = async (req, res) => {
    }
 }
 
-const pull2 = async (req, res) => {
-    try {
-        const response = await axios.get('localhost:7070/apprise')
-        console.log(response)
-    } catch (e) {
-        console.log(e)
-    }
-}
 
 module.exports = {  pull };
